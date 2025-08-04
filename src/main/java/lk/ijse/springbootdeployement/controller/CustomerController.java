32be9d6a518f4c31401851181331ca39050e9fe3
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-08-04 11:34:21 +0530

    Update Code

--- src/main/java/lk/ijse/springbootdeployement/controller/CustomerController.java
+++ src/main/java/lk/ijse/springbootdeployement/controller/CustomerController.java
@@ -16,7 +16,7 @@ public class CustomerController {
         return "Customer";
     }
 
-    @PostMapping
+    @PostMapping("/register")
     public ResponseEntity<Customer> saveCustomer(@RequestBody Customer customer){
         return ResponseEntity.ok(customerService.saveCustomer(customer));
     }

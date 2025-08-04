a5ad85c54a2624de2914b168e0c383058859de9b
kalana warnakulasooriya <kalana.warnakulasooriya@gmail.com>
2025-08-04 10:23:44 +0530

    Update

--- src/main/java/lk/ijse/springbootdeployement/CustomerController.java
+++ src/main/java/lk/ijse/springbootdeployement/CustomerController.java
@@ -1,4 +1,14 @@
 package lk.ijse.springbootdeployement;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customer")
 public class CustomerController {
+    @GetMapping
+    public String getCustomer(){
+        return "Customer";
+    }
 }
